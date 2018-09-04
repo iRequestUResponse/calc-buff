1280e6b28bf1675b57a7f36c75c83866169f84a7
Gear <nnote09@gmail.com>
2018-09-04 16:04:15 +0900

    아마도 자료만 다 입력하면 완성일듯.

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ Vue.component('equipment-component', {
 
 Vue.component('calc-proc-component', {
     template: '#calcProcComponent',
-    props: ['proc']
+    props: ['proc', 'buff']
 });
 
 let vm = new Vue({
@@ -34,7 +34,246 @@ let vm = new Vue({
             name: '크오빅',
             stat: null,
             check: false
-        }]
+        }],
+        buff: {
+            stat: 0,
+            name: "",
+            level: 0,
+            factor: function() {
+                let map = {
+                    영축: 630,
+                    용축: 700,
+                    아포: 750,
+                    크오빅: 830
+                }
+
+                return map[this.name];
+            },
+            ability: [],
+            result: {
+                힘: 0,
+                지능: 0,
+                물공: 0,
+                마공: 0,
+                독공: 0
+            },
+            calc: function() {
+                this.name = getProc();
+                let mapSkill = {};
+                for (let e in vm.model.skill) {
+                    mapSkill[vm.model.skill[e].name] = e;
+                }
+
+                let eq = vm.equipments[this.name]; // 현재 입고있는 장비
+                let setInfo = vm.model.set; // 모든 세트 정보
+                let setArray = {}; // 몇 세트 입고있는지
+
+                // setArray(객체)에 세트별로 몇 세트 입고있는지 저장
+                for (let e in eq) {
+                    let _setName = eq[e].set;
+                    if (_setName) {
+                        if (!setArray[_setName]) setArray[_setName] = 0;
+                        setArray[_setName]++;
+                    }
+                }
+
+                // this.ability(배열)에 ability push (세트)
+                for (let e in setInfo) {
+                    let _setName = setInfo[e].name;
+                    if (setArray[_setName]) { // 장착중인 장비 중 세트 있는지 확인
+                        let _ab = setInfo[e].ab;
+                        for (let ee in _ab) {
+                            if (_ab[ee].number <= setArray[_setNAme]) {
+                                for (let eee in _ab[ee].option) {
+                                    this.ability.push(_ab[ee].option[eee]);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // this.ability(배열)에 ability push (장비)
+                for (let e in eq) {
+                    for (let ee in eq[e].ab) {
+                        this.ability.push(eq[e].ab[ee]);
+                    }
+                }
+
+                // 레벨 초기화
+                this.level = vm.model.skill[mapSkill[this.name]].startlevel;
+
+                // 레벨 초기화 - 패시브 레벨 초기화
+                let passiveLevel = {};
+                switch (this.name) {
+                    case "영축":
+                    case "아포":
+                        passiveLevel = {
+                            보징: 0,
+                            신념: 0,
+                            디바인: 0
+                        };
+                        break;
+                    default:
+                        passiveLevel = {
+                            열정: 0
+                        };
+                        break;
+                };
+                let _skill = vm.model.skill;
+                for (let e in _skill) {
+                    let _skillName = _skill[e].name;
+                    if (passiveLevel[_skillName]) {
+                        passiveLevel[_skillName] = _skill[e].startlevel;
+                    }
+                }
+
+                // 레벨 증가
+                for (let e in this.ability) {
+                    let _parse = getAB(this.ability[e]);
+                    if (_parse.firstKey === "스탯") continue;
+                    if (_parse.secondKey !== "레벨") continue;
+                    if (passiveLevel[_parse.firstKey]) passiveLevel[_parse.firstKey] += _parse.val;
+                    if (_parse.firstKey === this.name) this.level += _parse.val;
+                }
+
+                let plusStat = {};
+                let multiStat = {};
+                for (let e in passiveLevel) {
+                    plusStat[e] = 0;
+                    multiStat[e] = 0;
+                }
+
+                for (let e in this.ability) {
+                    let _ab = getAB(this.ability[e]);
+                    if (plusStat[_ab.firstKey]) {
+                        if (_ab.isP) multiStat[_ab.firstKey] += _ab.val;
+                        else plusStat[_ab.firstKey] += _ab.val;
+                    }
+                }
+
+                // 스킬로 인한 스탯 증가치 plusStat에 더해주기
+                for (let e in plusStat) {
+                    let _sLevel = _skill[mapSkill[e]].startlevel;
+                    let _sAb = _skill[mapSkill[e]].ab;
+                    if (_sAb.length + _sLevel - 1 < passiveLevel[e]) {
+                        passiveLevel[e] = _sAb.length + _sLevel - 1;
+                    }
+
+                    plusStat[e] += _sAb[passiveLevel[e]].주스탯;
+                }
+
+                //
+                for (let e in multiStat) {
+                    plusStat[e] *= (multiStat[e] + 100) / 100;
+                }
+
+                for (let e in vm.proc) {
+                    if (vm.proc[e].name === this.name) {
+                        this.stat = vm.proc[e].stat;
+                    }
+                }
+
+                let applyStat = this.stat;
+
+                for (let e in plusStat) {
+                    applyStat += plusStat[e];
+                }
+
+                let _map = {
+                    영축: "체정",
+                    아포: "체정",
+                    용축: "지능",
+                    크오빅: "지능"
+                }
+
+                let applyMultiStat = 100;
+                for (let e in this.ability) {
+                    let _ab = getAB(this.ability[e]);
+                    if (_ab.firstKey === "스탯" && _ab.secondKey === _map[this.name]) {
+                        if (_ab.isP) {
+                            applyStat += _ab.val;
+                        } else {
+                            applyMultiStat += _ab.val;
+                        }
+                    }
+                }
+                applyStat *= applyMultiStat / 100;
+
+                // 스탯은 다 구함
+
+                let skillAb = vm.model.skill[mapSkill[this.name]].ab;
+                let _startlevel = vm.model.skill[mapSkill[this.name]].startlevel;
+                if (this.level > skillAb.length + _startlevel - 1) {
+                    this.level = skillAb.length + _startlevel - 1;
+                }
+
+                let _ability = skillAb[this.level - _startlevel];
+
+                for (let e in _ability) {
+                    if (e === "물마독") {
+                        this.result["물공"] = _ability[e];
+                        this.result["마공"] = _ability[e];
+                        this.result["독공"] = _ability[e];
+                    }
+                    if (e === "힘지") {
+                        this.result["힘"] = _ability[e];
+                        this.result["지능"] = _ability[e];
+                    }
+                }
+
+                // 이제 this.ability에서 getAB 통해서 this.result의 것들 값 더해준 뒤 곱해주기
+                let _percent = {
+                    힘: 100,
+                    지능: 100,
+                    물공: 100,
+                    마공: 100,
+                    독공: 100
+                };
+
+                for (let e in this.ability) {
+                    let _ab = getAB(this.ability[e]);
+                    if (_ab.firstKey === this.name && _ab.secondKey !== "레벨") {
+                        if (_ab.isP) {
+                            if (_ab.secondKey === "힘지") {
+                                _percent.힘 += _ab.val;
+                                _percent.지능 += _ab.val;
+                            } else if (_ab.secondKey === "물마공") {
+                                _percent.물공 += _ab.val;
+                                _percent.마공 += _ab.val;
+                            } else if (_ab.secondKey === "물마독") {
+                                _percent.물공 += _ab.val;
+                                _percent.마공 += _ab.val;
+                                _percent.독공 += _ab.val;
+                            } else {
+                                _percent[_ab.secondKey] += _ab.val;
+                            }
+                        } else {
+                            if (_ab.secondKey === "힘지") {
+                                this.result.힘 += _ab.val;
+                                this.result.지능 += _ab.val;
+                            } else if (_ab.secondKey === "물마공") {
+                                this.result.물공 += _ab.val;
+                                this.result.마공 += _ab.val;
+                            } else if (_ab.secondKey === "물마독") {
+                                this.result.물공 += _ab.val;
+                                this.result.마공 += _ab.val;
+                                this.result.독공 += _ab.val;
+                            } else {
+                                this.result[_ab.secondKey] += _ab.val;
+                            }
+                        }
+                    }
+                }
+
+                for (let e in _percent) {
+                    this.result[e] *= _percent[e] / 100;
+                }
+
+                for (let e in this.result) {
+                    this.result[e] = Math.round(this.result[e] * (1 + applyStat / this.factor()));
+                }
+            }
+        }
     }
 })
 
@@ -75,219 +314,7 @@ function getAB(STR) {
     return {
         firstKey: keyword[0],
         secondKey: keyword[1],
-        val: /[0-9]*/.exec(keyword[2])[0],
+        val: Number(/[0-9]*/.exec(keyword[2])[0]),
         isP: /%/.test(keyword[2])
     };
 }
-
-let buff = {
-    stat: 0,
-    name: "",
-    level: 0,
-    factor: function() {
-        let map = {
-            영축: 630,
-            용축: 700,
-            아포: 750,
-            크오빅: 830
-        }
-
-        return map[this.name];
-    },
-    type: 'male',
-    ability: [],
-    // 아리아 25% 잊지 말자
-    init: function() {
-        let _proc = getProc();
-        let mapSkill = {};
-        for (let e in vm.model.skill) {
-            mapSkill[vm.model.skill[e].name] = e;
-        }
-
-        let mapStat = {
-            영축: "체정",
-            아포: "체정",
-            용축: "지능",
-            크오빅: "지능"
-        };
-
-        let eq = vm.equipments[_proc];
-        let setInfo = vm.model.set;
-        // let setII = {};
-        // for (let e in setInfo) {
-        //     if (!setII[setInfo.name]) setII[setInfo.name] = 0;
-        //     setII[setInfo.name]++;
-        // }
-
-        // let ab_s = [];
-        // 정리 안 됨 ㅠ
-
-        // ability 정리(세트 포함)
-        for (let e in eq) {
-            for (let ee in eq[e].ab) {
-                ab_s.push(eq[e].ab[ee]);
-
-            }
-        }
-
-        // 레벨 초기화
-        this.level = vm.model.skill[mapSkill[_proc]].startlevel;
-        let passiveLevel = {
-            보징: 0,
-            신념: 0,
-            디바인: 0,
-            열정: 0
-        };
-        for (let e in ab_s) {
-            let _parse = getAB(ab_s[e]);
-            if (_parse.firstKey === "스탯") continue;
-            if (_parse.secondKey !== "레벨") continue;
-            if (passiveLevel[_parse.firstKey]) passiveLevel[_parse.firstKey] += _parse.val;
-            if (_parse.firstKey === _proc) this.level += _parse.val;
-        }
-    },
-    getAbility: function() {
-        // function ability() {
-        //     return {
-        //         statPlus: 0,
-
-        //     }
-        // }
-        // this.ability = {
-        //     statPlus: 0,
-        //     statPercent: 0,
-        //     skillLevel: 0,
-        //     skillPlus: {
-        //         힘: 0,
-        //         지능: 0,
-        //         물공: 0,
-        //         마공: 0,
-        //         독공: 0
-        //     },
-        //     skillPercent: {
-        //         힘: 0,
-        //         지능: 0,
-        //         물공: 0,
-        //         마공: 0,
-        //         독공: 0
-        //     }
-        // };
-        // let eq = vm._data.equipments;
-        // let _proc = getProc(vm.proc);
-        // for (let e in vm.proc) {
-        //     if (vm.proc[e].check) {
-        //         this.stat = vm.proc[e].stat;
-        //         this.name = vm.proc[e].name;
-        //         for (let ee in vm.model.skill) {
-        //             if (vm.model.skill[e].name === this.name) {
-        //                 this.level = vm.model.skill[e].startlevel;
-        //                 break;
-        //             }
-        //         }
-        //         break;
-        //     }
-        // }
-
-        // for (let e in eq[_proc]) {
-        //     let abArray = eq[_proc][e].ab;
-
-        //     for (let ee in abArray) {
-        //         this.abToTruth(abArray[ee], _proc);
-        //         // console.log(abArray[ee], typeof abArray[ee]);
-        //     }
-        // }
-
-        // console.log(this.name, this.stat, this.level, this.factor())
-    },
-    abToTruth: function(_string, _proc_) {
-        // let _ab = getAB(_string);
-        // let map = {
-        //     영축: "체정",
-        //     용축: "지능",
-        //     아포: "체정",
-        //     크오빅: "지능"
-        // };
-        // if (_ab.isP) { // precent일 경우
-        //     if (_ab.skillName === "스탯") { //
-        //         if (_ab.statName === map[_proc_]) {
-        //             this.ability.statPercent += _ab.val;
-        //         }
-        //     } else if (_ab.skillName === _proc_) { // 스킬 이름일 경우(영축, 용축, 아포, 크오빅 중 하나)
-        //         switch (_ab.statName) {
-        //             case "힘":
-        //             case "지능":
-        //             case "물공":
-        //             case "마공":
-        //             case "독공":
-        //                 this.ability.skillPercent[_ab.statName] += _ab.val;
-        //                 break;
-        //             case "힘지":
-        //                 this.ability.skillPercent["힘"] += _ab.val;
-        //                 this.ability.skillPercent["지능"] += _ab.val;
-        //                 break;
-        //             case "물마공":
-        //                 this.ability.skillPercent["물공"] += _ab.val;
-        //                 this.ability.skillPercent["마공"] += _ab.val;
-        //                 break;
-        //             case "물마독":
-        //                 this.ability.skillPercent["물공"] += _ab.val;
-        //                 this.ability.skillPercent["마공"] += _ab.val;
-        //                 this.ability.skillPercent["독공"] += _ab.val;
-        //                 break;
-        //         }
-        //     } else { // 스킬 이름인데 영축, 용축, 아포, 크오빅이 아닐 경우
-
-        //     }
-        // } else {
-        //     if (_ab.skillName === "스탯") { // 
-
-        //     } else if (_ab.skillName === _proc_) { // 스킬 이름일 경우(영축, 용축, 아포, 크오빅 중 하나)
-
-        //         // _ab.statName
-        //         // _ab.val
-        //     } else { // 스킬 이름인데 영축, 용축, 아포, 크오빅이 아닐 경우
-
-        //     }
-        // }
-    },
-    // getSkill: function() {
-
-    // },
-    // getStat: function() {
-    //     let stat = Number(this.stat);
-    //     let statPercent = 1;
-
-    //     for (let e in this.equipments) {
-    //         if (e.ab) {
-    //             let _ab = getAB(e.ab);
-
-    //             if (_ab.skillName === "스탯") {
-    //                 let map0 = {
-    //                     영축: "체정",
-    //                     용축: "지능",
-    //                     아포: "체정",
-    //                     크오빅: "지능"
-    //                 }
-
-    //                 if (_ab.statName === map0[this.name]) {
-    //                     if (_ab.isP) {
-    //                         statPercent += _ab.val;
-    //                     } else {
-    //                         stat += _ab.val;
-    //                     }
-    //                 }
-    //             } else {
-    //                 let map1 = {
-
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // 스킬 레벨 올리기
-    //     // 스킬 주스탯 올리기
-    //     // 주스탯 올리기
-    //     // 원래 주스탯에 스킬 주스탯 올리기
-    //     // 원래 주스탯에 장비 주스탯 올리기
-    //     // 원래 주스탯에 장비 주스탯 곱하기
-    // }
-};
